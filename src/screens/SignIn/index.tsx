500cb2671dd2d8f28e4b84564f2b6f6479c76748
joaoramos28 <joaogabrielramos28@outlook.com>
2022-09-05 23:28:47 -0300

    🎨 feat: Create Input Component

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,6 +1,17 @@
+import { Input } from "@components/Input";
 import React from "react";
 import { Container } from "./styles";
 
 export function SignIn() {
-  return <Container></Container>;
+  return (
+    <Container>
+      <Input
+        placeholder="E-mail"
+        type="secondary"
+        autoCorrect={false}
+        autoCapitalize="none"
+      />
+      <Input placeholder="Senha" type="secondary" secureTextEntry />
+    </Container>
+  );
 }
