3e38ff95fb8d2dc650563910c1680a0e925e0e4d
joaoramos28 <joaogabrielramos28@outlook.com>
2022-09-05 23:19:25 -0300

    💄 feat: Add status bar style

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ import AppLoading from "expo-app-loading";
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
 import { SignIn } from "@screens/SignIn";
+import { StatusBar } from "expo-status-bar";
 export default function App() {
   const [fontsLoaded] = useFonts({
     DMSans_400Regular,
@@ -18,6 +19,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
       <SignIn />
     </ThemeProvider>
   );
