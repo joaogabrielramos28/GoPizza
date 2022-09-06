dc541af8852a1582830bd462a04e4b8881c38c40
joaoramos28 <joaogabrielramos28@outlook.com>
2022-09-06 11:13:07 -0300

    🚧 feat: Product screen

--- App.tsx
+++ App.tsx
@@ -1,13 +1,18 @@
-import React from "react";
 import "react-native-gesture-handler";
+import React from "react";
+import { ActivityIndicator } from "react-native";
+import { StatusBar } from "expo-status-bar";
+
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans";
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display";
+
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
+
 import { SignIn } from "@screens/SignIn";
-import { StatusBar } from "expo-status-bar";
-import { ActivityIndicator } from "react-native";
+import { Product } from "@screens/Product";
 import { AuthProvider } from "@hooks/auth";
+
 export default function App() {
   const [isLoaded] = useFonts({
     DMSans_400Regular,
@@ -22,7 +27,7 @@ export default function App() {
     <ThemeProvider theme={theme}>
       <StatusBar style="light" translucent backgroundColor="transparent" />
       <AuthProvider>
-        <SignIn />
+        <Product />
       </AuthProvider>
     </ThemeProvider>
   );
