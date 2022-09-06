c0864dc18f42ecdb1f7461a814069e76fadb681e
joaoramos28 <joaogabrielramos28@outlook.com>
2022-09-06 12:09:21 -0300

    🚧 feat: Creating states for fields of form

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -23,6 +23,12 @@ import {
 
 export function Product() {
   const [image, setImage] = useState("");
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [priceSizeP, setPriceSizeP] = useState("");
+  const [priceSizeM, setPriceSizeM] = useState("");
+  const [priceSizeG, setPriceSizeG] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   async function handlePickerImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
 
@@ -60,7 +66,7 @@ export function Product() {
         <Form>
           <InputGroup>
             <Label>Nome</Label>
-            <Input />
+            <Input onChangeText={setName} value={name} />
           </InputGroup>
 
           <InputGroup>
@@ -68,17 +74,35 @@ export function Product() {
               <Label>Descrição</Label>
               <MaxCharacters>0 de 60 caracteres</MaxCharacters>
             </InputGroupHeader>
-            <Input multiline maxLength={60} style={{ height: 80 }} />
+            <Input
+              multiline
+              maxLength={60}
+              style={{ height: 80 }}
+              value={description}
+              onChangeText={setDescription}
+            />
           </InputGroup>
 
           <InputGroup>
             <Label>Tamanhos e preços</Label>
-            <InputPrice size="P" />
-            <InputPrice size="M" />
-            <InputPrice size="G" />
+            <InputPrice
+              size="P"
+              value={priceSizeP}
+              onChangeText={setPriceSizeP}
+            />
+            <InputPrice
+              size="M"
+              value={priceSizeM}
+              onChangeText={setPriceSizeM}
+            />
+            <InputPrice
+              size="G"
+              value={priceSizeG}
+              onChangeText={setPriceSizeG}
+            />
           </InputGroup>
 
-          <Button title="Cadastrar pizza" />
+          <Button title="Cadastrar pizza" isLoading={isLoading} />
         </Form>
       </ScrollView>
     </Container>
