07429c7f377e103476d630c630b07023df3b67e5
joaoramos28 <joaogabrielramos28@outlook.com>
2022-09-06 11:56:26 -0300

    🚧 feat: InputPrice component

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -3,6 +3,7 @@ import { Platform, TouchableOpacity } from "react-native";
 import * as ImagePicker from "expo-image-picker";
 import { ButtonBack } from "@components/ButtonBack";
 import { Photo } from "@components/Photo";
+import { InputPrice } from "@components/InputPrice";
 
 import {
   Container,
@@ -47,6 +48,10 @@ export function Product() {
           onPress={handlePickerImage}
         />
       </Upload>
+
+      <InputPrice size="P" />
+      <InputPrice size="M" />
+      <InputPrice size="G" />
     </Container>
   );
 }
