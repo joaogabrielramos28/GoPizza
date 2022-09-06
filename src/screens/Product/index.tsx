4962b663a06df7d0d943e17dd2e5da64f9ed9914
joaoramos28 <joaogabrielramos28@outlook.com>
2022-09-06 11:25:11 -0300

    🚧 feat: BackButton component

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,3 +1,4 @@
+import { ButtonBack } from "@components/ButtonBack";
 import React from "react";
 import { Platform, TouchableOpacity } from "react-native";
 
@@ -7,6 +8,7 @@ export function Product() {
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
       <Header>
+        <ButtonBack />
         <Title>Cadastrar</Title>
         <TouchableOpacity>
           <DeleteLabel>Deletar</DeleteLabel>
