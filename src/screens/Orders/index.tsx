4a5265b63406752ac956bf425498abdd3f7ec35b
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-09-07 00:17:10 -0300

    ✨ feat: Orders Screen

--- src/screens/Orders/index.tsx
+++ src/screens/Orders/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { FlatList } from "react-native";
+import { OrderCard } from "@components/OrderCard";
+import { ItemSeparator } from "@components/ItemSeparator";
 import { Container, Header, Title } from "./styles";
 
 export function Orders() {
@@ -7,6 +10,19 @@ export function Orders() {
       <Header>
         <Title>Pedidos Feitos</Title>
       </Header>
+
+      <FlatList
+        data={["1", "2", "3"]}
+        keyExtractor={(item) => item}
+        renderItem={({ item, index }) => <OrderCard index={index} />}
+        numColumns={2}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{
+          paddingHorizontal: 24,
+          paddingBottom: 125,
+        }}
+        ItemSeparatorComponent={() => <ItemSeparator />}
+      />
     </Container>
   );
 }
