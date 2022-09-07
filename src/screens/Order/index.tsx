0664ae5f69b6e6c0caba1990f5f732d0451cbebf
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-09-06 23:51:15 -0300

    ✨ feat: Finish order Screen

--- src/screens/Order/index.tsx
+++ src/screens/Order/index.tsx
@@ -1,14 +1,69 @@
-import { ButtonBack } from "@components/ButtonBack";
-import React from "react";
+import React, { useState } from "react";
 import { Platform } from "react-native";
-import { Container, Header } from "./styles";
+
+import { ButtonBack } from "@components/ButtonBack";
+import { RadioButton } from "@components/RadioButton";
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+import { PIZZA_TYPES } from "@utils/pizzaTypes";
+
+import {
+  Container,
+  ContentScroll,
+  Form,
+  FormRow,
+  Header,
+  InputGroup,
+  Label,
+  Photo,
+  Price,
+  Sizes,
+  Title,
+} from "./styles";
 
 export function Order() {
+  const [size, setSize] = useState("");
+
   return (
     <Container behavior={Platform.OS === "ios" ? "padding" : undefined}>
-      <Header>
-        <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
-      </Header>
+      <ContentScroll>
+        <Header>
+          <ButtonBack onPress={() => {}} style={{ marginBottom: 108 }} />
+        </Header>
+
+        <Photo source={{ uri: "https://github.com/joaogabrielramos28.png" }} />
+
+        <Form>
+          <Title>Nome da Pizza</Title>
+          <Label>Selecione um tamanho</Label>
+          <Sizes>
+            {PIZZA_TYPES.map((item) => (
+              <RadioButton
+                key={item.id}
+                title={item.name}
+                onPress={() => setSize(item.id)}
+                selected={size === item.id}
+              />
+            ))}
+          </Sizes>
+
+          <FormRow>
+            <InputGroup>
+              <Label>Número da mesa</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+            <InputGroup>
+              <Label>Quantidade</Label>
+              <Input keyboardType="numeric" />
+            </InputGroup>
+          </FormRow>
+
+          <Price>Valor de R$00,00</Price>
+
+          <Button title="Confirmar pedido" />
+        </Form>
+      </ContentScroll>
     </Container>
   );
 }
